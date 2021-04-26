82c8ad87f7739a236d796ed9ec481fec022b7799
legionkun <79984003+legionkun@users.noreply.github.com>
2021-04-26 20:13:18 +0700

    + initialize Source

---
+++ CoffeeMintClien/src/test/java/com/green/jdevd010/coffeemintclient/CoffeeMintClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.jdevd010.coffeemintclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMintClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
